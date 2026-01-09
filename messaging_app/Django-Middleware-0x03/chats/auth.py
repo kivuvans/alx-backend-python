fd0f3788fd7883cc1855848c6971db287a854187
evans kivuva <kivuvaevans@gmail.com>
2026-01-09 21:09:23 +0300

    replace messaging_app with local version

--- messaging_app/Django-Middleware-0x03/chats/auth.py
+++
@@ -1,9 +0,0 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-
-class CustomJWTAuthentication(JWTAuthentication):
-    """
-    Extends default JWTAuthentication if you want to customize later.
-    Currently just uses default behavior.
-    """
-    pass
