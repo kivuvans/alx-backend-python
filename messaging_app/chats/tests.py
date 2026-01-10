00f8c6feded72c86848cf39703a6816b04f1ecf2
evans kivuva <kivuvaevans@gmail.com>
2026-01-10 10:26:38 +0300

    Fix: converted messaging_app to a normal folder and added all files

---
+++ messaging_app/chats/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+# Simple test file for ALX requirements
+def test_addition():
+    assert 1 + 1 == 2
+
+
+def test_string():
+    assert "hello".upper() == "HELLO"
+
+
+def test_list():
+    assert len([1, 2, 3]) == 3
